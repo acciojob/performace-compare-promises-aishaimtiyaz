800ccf49c70a790dfbc5b4edb2bea0016e057a81
aishaimtiyaz <144771688+aishaimtiyaz@users.noreply.github.com>
2023-12-19 20:46:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -13,3 +13,34 @@ const apiUrls = [
 ];
 
 // You can write your code here
+// script.js
+
+// Function to fetch data using Promise.all
+const fetchDataAll = async () => {
+  const startTime = Date.now();
+  await Promise.all(apiUrls.map(url => fetch(url)));
+  const endTime = Date.now();
+  return endTime - startTime;
+};
+
+// Function to fetch data using Promise.any
+const fetchDataAny = async () => {
+  const startTime = Date.now();
+  await Promise.any(apiUrls.map(url => fetch(url)));
+  const endTime = Date.now();
+  return endTime - startTime;
+};
+
+// Function to display results on the webpage
+const displayResults = async () => {
+  const timeTakenAll = await fetchDataAll();
+  const timeTakenAny = await fetchDataAny();
+
+  // Update the table with the results
+  document.getElementById("output-all").textContent = timeTakenAll;
+  document.getElementById("output-any").textContent = timeTakenAny;
+};
+
+// Call the displayResults function when the window has loaded
+window.onload = displayResults;
+
